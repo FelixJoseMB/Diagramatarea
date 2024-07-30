5439c74c5a5f3e9764188938256f09e164ec003c
Kayle <Kayle@DESKTOP-020GO53>
2024-07-30 18:51:50 -0400

    Agregar archivos de proyecto.

---
+++ ConceptosPOOadiagrama.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+public class MiembroDeLaComunidad
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+
+    public MiembroDeLaComunidad(int id, string nombre, string apellido)
+    {
+        Id = id;
+        Nombre = nombre;
+        Apellido = apellido;
+    }
+}
+
+
+public class Empleado : MiembroDeLaComunidad
+{
+    public Empleado(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
+
+
+public class Estudiante : MiembroDeLaComunidad
+{
+    public Estudiante(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
+
+public class ExAlumno : MiembroDeLaComunidad
+{
+    public ExAlumno(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
+
+
+public class Docente : Empleado
+{
+    public Docente(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
+
+
+public class Administrativo : Empleado
+{
+    public Administrativo(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
+
+
+public class Administrador : Docente
+{
+    public Administrador(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
+
+
+public class Maestro : Docente
+{
+    public Maestro(int id, string nombre, string apellido)
+        : base(id, nombre, apellido) { }
+}
